a4435a2fe692996edf72c4cddc8e4f00e8a8e678
Sasikumar Lakshmanan <sasikumar@giffgaff.co.uk>
2020-10-17 21:20:04 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/sasi/flash_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sasi.flash_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final routes = {
+      WelcomeScreen.id: (context) => WelcomeScreen(),
+      LoginScreen.id: (context) => LoginScreen(),
+      RegistrationScreen.id: (context) => RegistrationScreen(),
+      ChatScreen.id: (context) => ChatScreen()
+    };
+
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: routes,
+    );
+  }
+}
